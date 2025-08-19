20bec164995fd5b232aa03557c18fd0ef2a739b2
VictorSantos6 <victor.santos6@upr.edu>
2025-08-19 18:45:19 -0400

    entities created in the domain

---
+++ lib/features/domain/repositories/flashcard_entity.dart
@@ -0,0 +1,7 @@
+class FlashcardEntity {
+  final String question;
+  final String response;
+  final String answer;
+
+  FlashcardEntity({required this.question, required this.response, required this.answer});
+}
--- lib/features/domain/repositories/flashcard_repository.dart
+++ lib/features/domain/repositories/flashcard_repository.dart
@@ -1,4 +1,6 @@
 
+import 'package:flash_cards_project/features/domain/repositories/flashcard_entity.dart';
+
 abstract class FlashcardRepository {
   Future<void> addFlashcard(FlashcardEntity card);
   Future<void> deleteFlashcard(String id);
